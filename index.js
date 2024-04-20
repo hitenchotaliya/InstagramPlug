c80c1c389dbf747f9d622023ef0ec171af93da02
CHOTALIYA HITEN <chotaliyahiten672@gmail.com>
2024-04-20 12:16:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,9 @@ app.get('/getCode', async (req, res) => {
         longLivedToken = tokenResponse.data.access_token;
 
         // Redirect back to homepage after storing the tokens
-        res.redirect('/');
+       // res.redirect('/');
+                res.redirect('http://localhost/xyz');
+
     } catch (error) {
         console.error('Error:', error);
         res.status(500).send('Internal Server Error');
